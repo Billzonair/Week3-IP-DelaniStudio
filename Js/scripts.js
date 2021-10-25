d698bc958be69a172e2248b92f2527c37c645b92
Billy Carnell <billy.dushime@student.moringaschool.com>
2021-10-25 20:10:02 +0300

    i was able to fix the toggle option however the hover code for the portfolio is still not working

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-    $(".design").click(function() {
-      $(".design-showing").toggle();
-      $(".design-hidden").toggle();
-    });
-  });
+  $(".design-image").click(function(){
+    $(".design-showing").toggle();
+    $(".design-hidden").toggle();
+  })
+})
+$(document).ready(function() {
+  $(".development-image").click(function(){
+    $(".development-showing").toggle();
+    $(".development-hidden").toggle();
+  })
+})
+$(document).ready(function() {
+  $(".product-management-image").click(function(){
+    $(".product-management-showing").toggle();
+    $(".product-management-hidden").toggle();
+  })
+})
+
