b4333895b57f15b92898b0b9e456104400ea0768
Billy Carnell <billy.dushime@student.moringaschool.com>
2021-10-27 19:24:33 +0300

    I have added the hover effect on the portfolio part

--- Js/scripts.js
+++ Js/scripts.js
@@ -19,6 +19,64 @@ $(document).ready(function() {
 function submit(){
   alert("we have received your message. Thank you for reaching out to us.")
 }
+$(document).ready(function(){
+  $("#work-1").hover(function(){
+    $("#portfolio-1").show();
+    }, function(){
+      $("#portfolio-1").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work-2").hover(function(){
+    $("#portfolio-2").show();
+    }, function(){
+      $("#portfolio-2").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work-3").hover(function(){
+    $("#portfolio-3").show();
+    }, function(){
+      $("#portfolio-3").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work-4").hover(function(){
+    $("#portfolio-4").show();
+    }, function(){
+      $("#portfolio-4").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work-5").hover(function(){
+    $("#portfolio-5").show();
+    }, function(){
+      $("#portfolio-5").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work-6").hover(function(){
+    $("#portfolio-6").show();
+    }, function(){
+      $("#portfolio-6").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work-7").hover(function(){
+    $("#portfolio-7").show();
+    }, function(){
+      $("#portfolio-7").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work-8").hover(function(){
+    $("#portfolio-8").show();
+    }, function(){
+      $("#portfolio-8").hide();
+  });
+});
+
+
 
 
 
