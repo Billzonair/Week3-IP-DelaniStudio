a9901efe01995bca87e5f7baa786dc5410151253
Billy Carnell <billy.dushime@student.moringaschool.com>
2021-10-27 11:26:28 +0300

    I edited the what we do section as requested in the comment

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-  $(".design-image").click(function(){
+  $(".design-text").click(function(){
     $(".design-showing").toggle();
     $(".design-hidden").toggle();
   })
 })
 $(document).ready(function() {
-  $(".development-image").click(function(){
+  $(".development-text").click(function(){
     $(".development-showing").toggle();
     $(".development-hidden").toggle();
   })
 })
 $(document).ready(function() {
-  $(".product-management-image").click(function(){
+  $(".product-management-text").click(function(){
     $(".product-management-showing").toggle();
     $(".product-management-hidden").toggle();
   })
