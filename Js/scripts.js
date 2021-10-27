7d66f0056b665a7465a019d5e521f6134e66b185
Billy Carnell <billy.dushime@student.moringaschool.com>
2021-10-27 20:19:46 +0300

    I have adjusted some fonts-sizes to make some texts clearer

--- Js/scripts.js
+++ Js/scripts.js
@@ -21,56 +21,56 @@ function submit(){
 }
 $(document).ready(function(){
   $("#work-1").hover(function(){
-    $("#portfolio-1").show();
+    $("#portfolio-1").show(200);
     }, function(){
       $("#portfolio-1").hide();
   });
 });
 $(document).ready(function(){
   $("#work-2").hover(function(){
-    $("#portfolio-2").show();
+    $("#portfolio-2").show(200);
     }, function(){
       $("#portfolio-2").hide();
   });
 });
 $(document).ready(function(){
   $("#work-3").hover(function(){
-    $("#portfolio-3").show();
+    $("#portfolio-3").show(200);
     }, function(){
       $("#portfolio-3").hide();
   });
 });
 $(document).ready(function(){
   $("#work-4").hover(function(){
-    $("#portfolio-4").show();
+    $("#portfolio-4").show(200);
     }, function(){
       $("#portfolio-4").hide();
   });
 });
 $(document).ready(function(){
   $("#work-5").hover(function(){
-    $("#portfolio-5").show();
+    $("#portfolio-5").show(200);
     }, function(){
       $("#portfolio-5").hide();
   });
 });
 $(document).ready(function(){
   $("#work-6").hover(function(){
-    $("#portfolio-6").show();
+    $("#portfolio-6").show(200);
     }, function(){
       $("#portfolio-6").hide();
   });
 });
 $(document).ready(function(){
   $("#work-7").hover(function(){
-    $("#portfolio-7").show();
+    $("#portfolio-7").show(200);
     }, function(){
       $("#portfolio-7").hide();
   });
 });
 $(document).ready(function(){
   $("#work-8").hover(function(){
-    $("#portfolio-8").show();
+    $("#portfolio-8").show(200);
     }, function(){
       $("#portfolio-8").hide();
   });
